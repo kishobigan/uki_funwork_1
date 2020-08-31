60e3ac4d89647f3294a467d517f8066095eb803f
kishobigan <karunairasa.kishobigan@gmail.com>
2020-08-31 21:23:46 +0530

    funwork1

---
+++ main.js
@@ -0,0 +1,170 @@
+SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/316972940',function(sound){
+      $('#start').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/317968951',function(sound){
+      $('#start2').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop2').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/94431903',function(sound){
+      $('#start3').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop3').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/321563322',function(sound){
+      $('#start4').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop4').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/317968665',function(sound){
+      $('#start5').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop5').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/305249637',function(sound){
+      $('#start6').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop6').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/349457705',function(sound){
+      $('#start7').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop7').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/181296381',function(sound){
+      $('#start8').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop8').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/84237364',function(sound){
+      $('#start9').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop9').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/304926623s',function(sound){
+      $('#start10').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop10').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+
+  $(document).ready(function() {
+    SC.stream('/tracks/311975855',function(sound){
+      $('#start11').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop11').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
+
+  $(document).ready(function() {
+    SC.stream('/tracks/345946410',function(sound){
+      $('#start12').click(function(e){
+        e.preventDefault();
+        sound.start();
+      });
+      $('#stop').click(function(e){
+        e.preventDefault();
+        sound.stop();
+      });
+    });
+    });
